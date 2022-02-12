74bbe6b9545b41f4af47cf6bc9ef1c85ec5a3813
Poojani Herath <92888@fhss.sjp.ac.lk>
2022-02-12 15:53:55 +0530

    initial commit

---
+++ variables.js
@@ -0,0 +1,20 @@
+// Comments ( // single line or /* multi line comment*/ )
+
+// case sensitive
+var myScore = 150
+var myscore = 200
+
+myScore
+myscore
+
+// can't start with a number
+// var 1stScore = 260;
+
+// can start with $ or underscore(_)
+var $ = 89
+var _subject1 = 78
+
+$
+_subject1
+
+// camelCase is using for create variable names
